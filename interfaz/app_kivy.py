e26066a50d0310d016d13cae1cff05e77659a836
Esteban Bernal <estebanbernalmonroyedc@gmail.com>
2026-03-13 17:28:13 -0500

    habia un error al iniciar la aplicacion, ya lo corregi

--- interfaz/app_kivy.py
+++ interfaz/app_kivy.py
@@ -1,32 +1,38 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from codigo.main import crear_recordatorio
 
+
 class Pantalla(BoxLayout):
 
     def __init__(self, **kwargs):
         super().__init__(orientation="vertical", **kwargs)
 
-        self.titulo=TextInput(hint_text="Titulo")
+        self.titulo = TextInput(hint_text="Titulo")
         self.add_widget(self.titulo)
 
-        self.descripcion=TextInput(hint_text="Descripcion")
+        self.descripcion = TextInput(hint_text="Descripcion")
         self.add_widget(self.descripcion)
 
-        self.fecha=TextInput(hint_text="Fecha (YYYY-MM-DD)")
+        self.fecha = TextInput(hint_text="Fecha (YYYY-MM-DD)")
         self.add_widget(self.fecha)
 
-        self.hora=TextInput(hint_text="Hora (HH:MM)")
+        self.hora = TextInput(hint_text="Hora (HH:MM)")
         self.add_widget(self.hora)
 
-        boton=Button(text="Guardar recordatorio")
+        boton = Button(text="Guardar recordatorio")
         boton.bind(on_press=self.guardar)
 
         self.add_widget(boton)
 
-    def guardar(self,obj):
+    def guardar(self, obj):
 
         crear_recordatorio(
             self.titulo.text,
@@ -37,9 +43,11 @@ class Pantalla(BoxLayout):
 
         print("Recordatorio guardado")
 
+
 class MiRecordatorioApp(App):
 
     def build(self):
         return Pantalla()
+
 
 MiRecordatorioApp().run()
